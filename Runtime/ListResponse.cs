a3e899a1d3350570de88eb3eeaa1fd73f131fdb2
Stephen Hodgson <hodgson.designs@gmail.com>
2021-07-04 17:35:37 -0700

    Better handling of directory resources (#5)
    - adds the ability to list resources in a specified directory/path.
    - better handling of child resources

---
+++ Runtime/ListResponse.cs
@@ -0,0 +1,27 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Firebase.Storage
+{
+    [Serializable]
+    internal class ListResponse
+    {
+        [SerializeField]
+        private string kind;
+
+        public string Kind => kind;
+
+        [SerializeField]
+        private List<string> prefixes;
+
+        public IReadOnlyList<string> Prefixes => prefixes;
+
+        [SerializeField]
+        private List<StorageObjectMetaData> items;
+
+        public IReadOnlyList<StorageObjectMetaData> Items => items;
+    }
+}
---
+++ Runtime/StorageObjectMetaData.cs
@@ -0,0 +1,79 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using UnityEngine;
+
+namespace Firebase.Storage
+{
+    /// <summary>
+    /// Full list of meta data available here: https://firebase.google.com/docs/storage/web/file-metadata
+    /// </summary>
+    [Serializable]
+    public class StorageObjectMetaData
+    {
+        [SerializeField]
+        private string name;
+
+        public string Name => name;
+
+        [SerializeField]
+        private string bucket;
+
+        public string Bucket => bucket;
+
+        [SerializeField]
+        private string kind;
+
+        public string Kind => kind;
+
+        [SerializeField]
+        private string id;
+
+        public string Id => id;
+
+        [SerializeField]
+        private string selfLink;
+
+        public string SelfLink => selfLink;
+
+        [SerializeField]
+        private string mediaLink;
+
+        private string MediaLink => mediaLink;
+
+        [SerializeField]
+        private string contentType;
+
+        private string ContentType => contentType;
+
+        [SerializeField]
+        private string size;
+
+        private int Size => int.Parse(size);
+
+        [SerializeField]
+        private string timeCreated;
+
+        public DateTime TimeCreate => DateTime.Parse(timeCreated);
+
+        [SerializeField]
+        private string updated;
+
+        public DateTime Updated => DateTime.Parse(updated);
+
+        [SerializeField]
+        private string md5Hash;
+
+        public string Md5Hash => md5Hash;
+
+        [SerializeField]
+        private string contentEncoding;
+
+        public string ContentEncoding => contentEncoding;
+
+        [SerializeField]
+        private string contentDisposition;
+
+        public string ContentDisposition => contentDisposition;
+    }
+}
